fd51a5271e45406bae1fcc7d3bac44fa7ecf4848
made777 <maduri.e7@gmail.com>
2020-06-13 19:52:11 +1000

    implemented getConfig

--- dd_tool/server/server.py
+++ dd_tool/server/server.py
@@ -30,3 +30,37 @@ class Server(Page):
   @cherrypy.expose
   def index(self):
     return self.seedcrawler()
+
+@cherrypy.expose
+  def thing(self):
+    cherrypy.response.headers['Content-Type'] = 'text/plain'
+    def content():
+      print "\n\n\nRUNNING CONTENT\n\n\n"
+      yield json.dumps({"first":"Hello"})
+      yield json.dumps({"first":"World"})
+    return content()
+  thing._cp_config = {'response.stream': True}
+
+if __name__ == "__main__":
+  server = Server()
+  app = cherrypy.quickstart(server, config=Server.getConfig())
+  cherrypy.config.update(server.config)
+else:
+  server = Server()
+  config = Server.getConfig()
+  app = cherrypy.tree.mount(server, config=config)
+
+  @staticmethod
+  def getConfig():
+    config = ConfigParser()
+    config.read(os.path.join(os.path.dirname(__file__), "config.conf"))
+    configMap = {}
+    for section in config.sections():
+      configMap[section] = {}
+      for option in config.options(section):
+        val = config.get(section, option)
+        if option == "server.socket_port" or option == "server.thread_pool":
+          val = int(val)
+        configMap[section][option] = val
+
+    return configMap
