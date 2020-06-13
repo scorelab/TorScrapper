f071b1f1990522c29d71c3a20fee6dcb1b761e2f
made777 <maduri.e7@gmail.com>
2020-06-13 19:12:01 +1000

    added initial init for server

---
+++ dd_tool/server/server.py
@@ -0,0 +1,21 @@
+import urlparse
+from domain_discovery_API.models.domain_discovery_model import DomainModel
+from domain_discovery_API.models.crawler_model import CrawlerModel
+from domain_discovery_API.models.RadvizModel import RadvizModel
+import json
+import os
+from threading import Lock
+import cherrypy
+from domain_discovery_API.server import Page
+from ConfigParser import ConfigParser
+
+class Server(Page):
+  def __init__(self):
+    path = os.path.dirname(os.path.realpath(__file__))
+    self._ddtModel = DomainModel(path)
+    self._crawlerModel = CrawlerModel(path)
+    self._ddtModel.setCrawlerModel(self._crawlerModel)
+    self._radvizModel = RadvizModel(path)
+    models = {"domain": self._ddtModel, "crawler": self._crawlerModel, "radviz": self._radvizModel}
+    super(Server, self).__init__(models, path)
+
