5117e7454aa871251a3465816fa55fa7ce2f283e
made777 <maduri.e7@gmail.com>
2020-06-21 12:15:10 +1000

    created the handlers and domain methods

--- dd_tool/client/components/Home.js
+++ dd_tool/client/components/Home.js
@@ -31,6 +31,7 @@ const styles = {
 
 
 class Home extends Component {
+
   constructor(props){
     super(props);
     this.state = {
@@ -45,8 +46,129 @@ class Home extends Component {
     this.textInput = null;
   }
 
+  addDelDomains(id,name){
+    var tempDelDomains = this.state.delDomains;
+    tempDelDomains[id]=name;
+    this.setState({delDomains:tempDelDomains});
+  }
+
+  deleteDomains(){
+    var delDomains= this.state.delDomains;
+    $.post(
+      '/delDomain',
+      {'domains': JSON.stringify(delDomains)},
+      function(domains) {
+        this.setState({openDeleteDomain: false, delDomains: {}});
+        this.getAvailableDomains();
+        this.forceUpdate();
+      }.bind(this)
+    );
+  };
+
+  getAvailableDomains(){
+    $.post(
+      '/getAvailableDomains',
+      {"type": "init"},
+      function(domains) {
+        this.setState({domains: domains['crawlers']});
+      }.bind(this)
+    );
+  }
+  componentWillMount() {
+    this.getAvailableDomains();
+  }
+
+  
+   focusTextField() {
+     setTimeout(() => this.textInput.focus(), 100);
+   }
+
+
+  createNewDomain(){
+    var nameDomain= this.state.newNameDomain;
+    var duplicateDomain = false;
+    var mydata = this.state.domains;
+    Object.keys(mydata).map((k, index)=>{ var name = mydata[k].name; if(name.trim().toLowerCase().replace(/\s+/g,"_") === nameDomain.trim().toLowerCase().replace(/\s+/g,"_")){ duplicateDomain = true;} });   // .trim() to remove last and first spaces from a string. 
+    if(!duplicateDomain){
+      $.post(
+        '/addDomain',
+        {'index_name': nameDomain},
+        function(domains) {
+          this.setState({openCreateDomain: false, newNameDomain:"", openDuplicateDomainName:false });
+          this.getAvailableDomains();
+          this.forceUpdate();
+        }.bind(this)
+      );
+    }
+    else{
+      this.setState({openCreateDomain: true, newNameDomain:nameDomain, openDuplicateDomainName:true});
+    }
+  };
+
+
+
+  handleCloseDeleteDomain = () => {
+    this.setState({openDeleteDomain: false});
+  };
+
+  handleCloseCreateDomain = () => {
+    this.setState({openCreateDomain: false, openDuplicateDomainName:false, newNameDomain:"" });
+  };
+
+  handleOpenDeleteDomain = () => {
+    this.setState({openDeleteDomain: true});
+  };
+
+  handleCloseDuplicateDomainName = () => {
+    this.setState({openDuplicateDomainName: false});
+  };
+
+  handleTextChangeNewNameDomain(e){
+    this.setState({ "newNameDomain": e.target.value});
+  }
+
+  handleOpenCreateDomain = () => {
+    this.setState({openCreateDomain: true});
+    this.focusTextField();
+  };
+
+  
   render(){
-      <div>Home</div>
+       const actionsCreateDomain = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleCloseCreateDomain}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.createNewDomain.bind(this)}
+      />,
+    ];
+
+    const actionsDeleteDomain = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleCloseDeleteDomain}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.deleteDomains.bind(this)}
+      />,
+    ];
+
+    const actionsDuplicateDomainName = [
+      <FlatButton
+        label="Ok"
+        primary={true}
+        onTouchTap={this.handleCloseDuplicateDomainName}
+      />,
+    ];
   }
 
 }
