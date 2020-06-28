8005da6e5e72627ed7ade439acdf00022b0241d8
made777 <maduri.e7@gmail.com>
2020-06-28 17:16:54 +1000

    created the domain component

---
+++ dd_tool/client/components/Domain.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Body from './Body';
+import Header from './Header';
+
+class Domain extends Component {
+
+    render() {
+
+	return (
+		<div>
+            <h1>Domain</h1>
+		</div>
+	);
+    }
+}
+
+export default Domain;
