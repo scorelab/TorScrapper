7b030bbd84a4ed4a338b8180e0df895cb8b460f6
dande9304 <dande9304@gmail.com>
2020-07-11 13:30:22 +1000

    added get term statics

--- dd_tool/api/get_mtermvectors_multi_prov.py
+++ dd_tool/api/get_mtermvectors_multi_prov.py
@@ -31,3 +31,53 @@ def terms_from_es_json(doc, rm_stopwords=True, rm_numbers=True, termstatistics =
         terms = { term: {'tf': docterms[term]} for term in valid_words }
 
     return terms
+
+def getTermStatistics(all_hits, rm_stopwords=True, rm_numbers=True, pos_tags=[], mapping=None, es_index='memex', es_doc_type='page', es=None):
+    if es is None:
+        es = default_es
+
+    executor = concurrent.futures.ProcessPoolExecutor(10)
+
+    futures = []
+    for i in range(0, len(all_hits), 10):
+        hits = all_hits[i:i+10]
+
+        futures.append(executor.submit(computeTermStats, hits))
+    concurrent.futures.wait(futures)    
+    
+    tfidfs = []
+    for stat in stats:
+        tfidf={}
+        for k in stat.keys():
+            tfidf[k] =stat[k]['tfidf']
+        tfidfs.append(tfidf)
+
+    tfs = []
+    for stat in stats:
+        tf={}
+        for k in stat.keys():
+            tf[k] =stat[k]['tf']
+        tfs.append(tf)
+    
+    v_tfidf = DictVectorizer()
+    v_tf = DictVectorizer()
+
+    data = v_tfidf.fit_transform(tfidfs).toarray()
+    corpus = v_tfidf.get_feature_names()
+    tf_data = v_tf.fit_transform(tfs).toarray()
+    
+    if len(pos_tags) > 0:
+        filtered_words = pos_filter(pos_tags, corpus)
+        indices = [corpus.index(term) for term in corpus if term not in filtered_words]
+        corpus =  np.delete(corpus, indices)
+        corpus = corpus.tolist()
+        data =  np.delete(data, indices, 1)
+        tf_data =  np.delete(tf_data, indices, 1)
+        ttf = {key:value for key, value in ttf.iteritems() if key in corpus}
+
+    result = [data, tf_data, ttf, corpus, docs]
+
+    del tfidfs
+    del tfs
+    
+    return result
