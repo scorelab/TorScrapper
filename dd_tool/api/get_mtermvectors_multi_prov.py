068947c209768b7768240800796eeb66b79eaf0e
dande9304 <dande9304@gmail.com>
2020-07-11 13:31:46 +1000

    added get term frequency

--- dd_tool/api/get_mtermvectors_multi_prov.py
+++ dd_tool/api/get_mtermvectors_multi_prov.py
@@ -102,3 +102,57 @@ def computeTermStats(hits):
                 for k in res.keys():
                     ttf[k] = res[k]['ttf']
     return [stats, docs, ttf]
+
+
+def pos_filter(pos_tags=['NN', 'NNS', 'NNP', 'NNPS', 'VBN', 'JJ'], docterms=[]):
+    tagged = nltk.pos_tag(docterms)
+    valid_words = [tag[0] for tag in tagged if tag[1] in pos_tags]
+    return valid_words
+
+def tfidf(tf, df, n_doc):
+    idf = math.log(n_doc / float(df))
+    return tf * idf
+
+
+def getTermFrequency(all_hits, rm_stopwords=True, rm_numbers=True, pos_tags=[], mapping=None, es_index='memex', es_doc_type='page', es=None):
+    if es is None:
+        es = default_es
+
+    docs = []
+    stats = []
+    corpus = []
+
+    once = True
+    for i in range(0, len(all_hits), 10):
+        hits = all_hits[i:i+10]
+
+        term_res = es.mtermvectors(index=es_index,
+                                   doc_type=es_doc_type,
+                                   fields=mapping['text'], 
+                                   ids=hits) 
+
+        for doc in term_res['docs']:
+            if doc.get('term_vectors'):
+                if mapping['text'] in doc['term_vectors']:
+                    docs.append(doc['_id'])
+                    res = terms_from_es_json(doc=doc, rm_stopwords=rm_stopwords, rm_numbers=rm_numbers,  mapping=mapping)
+                    stats.append(res)
+
+    tfs = []
+    for stat in stats:
+        tf={}
+        for k in stat.keys():
+            tf[k] =stat[k]['tf']['term_freq']
+        tfs.append(tf)
+
+    v_tf = DictVectorizer()
+    data = v_tf.fit_transform(tfs).toarray()
+    corpus = v_tf.get_feature_names()
+
+    if len(pos_tags) > 0:
+        filtered_words = pos_filter(pos_tags, corpus)
+        indices = [corpus.index(term) for term in corpus if term not in filtered_words]
+        corpus =  np.delete(corpus, indices)
+        data =  np.delete(data, indices, 1)
+
+    return [data, corpus.tolist(), docs]
