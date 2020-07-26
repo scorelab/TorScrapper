c504969e88a12369194b9d91f17aa75f86f532e8
dande9304 <dande9304@gmail.com>
2020-07-26 10:22:55 +1000

    added createModel

--- dd_tool/api/models/crawler_model.py
+++ dd_tool/api/models/crawler_model.py
@@ -71,3 +71,149 @@ class CrawlerModel():
 
     def getCrawlerServers(self):
         return self._crawler_monitors
+
+
+    def createModel(self, session=None, zip=True):
+        """ Create an ACHE model to be applied to SeedFinder and focused crawler.
+        It saves the classifiers, features, the training data in the <project>/data/<domain> directory.
+        If zip=True all generated files and folders are zipped into a file.
+
+        Parameters:
+        session (json): should have domainId
+
+        Returns:
+        Zip file url or message text
+        """
+        path = self._path
+
+        es_info = self._esInfo(session["domainId"])
+
+        data_dir = path + "/data/"
+        data_domain  = data_dir + es_info['activeDomainIndex']
+        data_training = data_domain + "/training_data/"
+        data_negative = data_domain + "/training_data/negative/"
+        data_positive = data_domain + "/training_data/positive/"
+
+        if (not isdir(data_positive)):
+            # Create dir if it does not exist
+            makedirs(data_positive)
+        else:
+            # Remove all previous files
+            for filename in listdir(data_positive):
+                remove(data_positive+filename)
+
+        if (not isdir(data_negative)):
+            # Create dir if it does not exist
+            makedirs(data_negative)
+        else:
+            # Remove all previous files
+            for filename in listdir(data_negative):
+                remove(data_negative+filename)
+
+        pos_tags = ["Relevant"]
+        neg_tags = ["Irrelevant"]
+
+        try:
+            pos_tags = session['model']['positive']
+        except KeyError:
+            print "Using default positive tags"
+
+        try:
+            neg_tags = session['model']['negative']
+        except KeyError:
+            print "Using default negative tags"
+
+        pos_docs = []
+
+        for tag in pos_tags: #.split(','):
+            s_fields = {}
+            query = {
+                "wildcard": {es_info['mapping']["tag"]:tag}
+            }
+            s_fields["queries"] = [query]
+
+            results = multifield_term_search(s_fields,
+                                             0, self._all,
+                                             ["url", es_info['mapping']['html']],
+                                             es_info['activeDomainIndex'],
+                                             es_info['docType'],
+                                             self._es)
+
+            pos_docs = pos_docs + results['results']
+
+        pos_html = {field['url'][0]:field[es_info['mapping']["html"]][0] for field in pos_docs}
+
+        neg_docs = []
+        for tag in neg_tags: #.split(','):
+            s_fields = {}
+            query = {
+                "wildcard": {es_info['mapping']["tag"]:tag}
+            }
+            s_fields["queries"] = [query]
+            results = multifield_term_search(s_fields,
+                                             0, self._all,
+                                             ["url", es_info['mapping']['html']],
+                                             es_info['activeDomainIndex'],
+                                             es_info['docType'],
+                                             self._es)
+            neg_docs = neg_docs + results['results']
+
+        neg_html = {field['url'][0]:field[es_info['mapping']["html"]][0] for field in neg_docs}
+
+        seeds_file = data_domain +"/seeds.txt"
+        print "Seeds path ", seeds_file
+        with open(seeds_file, 'w') as s:
+            for url in pos_html:
+                try:
+                    file_positive = data_positive + self._encode(url.encode('utf8'))
+                    s.write(url.encode('utf8') + '\n')
+                    with open(file_positive, 'w') as f:
+                        f.write(pos_html[url].encode('utf8'))
+
+                except IOError:
+                    _, exc_obj, tb = exc_info()
+                    f = tb.tb_frame
+                    lineno = tb.tb_lineno
+                    filename = f.f_code.co_filename
+                    linecache.checkcache(filename)
+                    line = linecache.getline(filename, lineno, f.f_globals)
+                    print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
+        for url in neg_html:
+            try:
+                file_negative = data_negative + self._encode(url.encode('utf8'))
+                with open(file_negative, 'w') as f:
+                    f.write(neg_html[url].encode('utf8'))
+            except IOError:
+                _, exc_obj, tb = exc_info()
+                f = tb.tb_frame
+                lineno = tb.tb_lineno
+                filename = f.f_code.co_filename
+                linecache.checkcache(filename)
+                line = linecache.getline(filename, lineno, f.f_globals)
+                print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
+        domainmodel_dir = data_domain + "/models/"
+
+        if (not isdir(domainmodel_dir)):
+            makedirs(domainmodel_dir)
+        else:
+            # Remove all previous files
+            for filename in listdir(domainmodel_dir):
+                remove(domainmodel_dir+filename)
+
+
+        if len(neg_docs) > 0:
+            ache_home = environ['ACHE_HOME']
+            comm = ache_home + "/bin/ache buildModel -t " + data_training + " -o "+ domainmodel_dir + " -c " + ache_home + "/config/sample_config/stoplist.txt"
+            p = Popen(comm, shell=True, stderr=PIPE)
+            output, errors = p.communicate()
+            print output
+            print errors
+        else:
+            return "No irrelevant pages to build domain model"
+
+        if zip:
+            return self._createModelZip(session)
+
+        return "Model created successfully"
