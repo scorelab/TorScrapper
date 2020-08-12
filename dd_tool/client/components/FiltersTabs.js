c2e60108a815fb5ec3e47c425e551dc129a08f34
made777 <maduri.e7@gmail.com>
2020-08-12 10:15:10 +1000

    created FiltersTabs

---
+++ dd_tool/client/components/FiltersTabs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Export from 'material-ui/svg-icons/file/file-download';
+import Dialog from 'material-ui/Dialog';
+import ActionAutorenew from 'material-ui/svg-icons/action/autorenew';
+import FlatButton from 'material-ui/FlatButton';
+import $ from 'jquery';
+import CircularProgress from 'material-ui/CircularProgress';
+import SwipeableViews from 'react-swipeable-views';
+import CheckboxTree from 'react-checkbox-tree';
+import IconButton from 'material-ui/IconButton';
+
+class FiltersTabs extends React.Component{
+    render(){
+        return(
+            <div>
+                FiltersTabs
+            </div>
+        )
+    }
+}
